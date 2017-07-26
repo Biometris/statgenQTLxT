9cfa2bec71d673db6a11b0c9233e16738fb49d60
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2017-07-26 15:13:20 +0200

    Modified gData

--- R/addPhenoData.R
+++ R/addPhenoData.R
@@ -12,16 +12,8 @@
 #' @return the input object of class \code{\link{gData}} with the imported phenotypic data in the
 #' component pheno.
 
-## TO DO: Complete the genotype names?
-## TO DO: addVarMeans
-## TO DO: addNormalTransform
-## TO DO: missings in factor columns
-
 addPhenoData  <- function(gData,
   pheno,
-  addVarMeans = FALSE,
-  meanCols = 0,
-  addNormalTransform = FALSE,
   factorCols = NULL) {
 
   if (missing(gData) || !is.gData(gData))
@@ -33,9 +25,6 @@ addPhenoData  <- function(gData,
   if (!is.null(factorCols) && !is.character(factorCols) && !is.numeric(factorCols))
     stop("factorCols should be either NULL, a vector of strings or a vector of integers\n")
 
-  if (!addVarMeans) {meanCols <- 0}
-  if (sum(meanCols)==0) {addVarMeans<-FALSE}
-
   ## Read only the first line of data to get the column names.
   phenoIn <- read.table(file = pheno, header = TRUE, sep = ",", nrows = 1)
   ## Set the column classes.
@@ -44,7 +33,7 @@ addPhenoData  <- function(gData,
   if (!is.null(factorCols)) {
     if (is.character(factorCols) && !all(factorCols %in% colnames(phenoIn))) {
       stop("all factorCols should be column names in pheno")
-    } else if (is.numeric(factorCols) && !all(factorCols <= ncol(phenoIn))) {
+    } else if (is.numeric(factorCols) && any(factorCols > ncol(phenoIn))) {
       stop("all factorCols should be columns in pheno")
     }
     factorPos <- ifelse(is.character(factorCols), which(colnames(phenoIn) %in% factorCols), factorCols)
@@ -52,53 +41,14 @@ addPhenoData  <- function(gData,
   }
   ## Read the full table
   phenoIn <- read.table(file = pheno, sep=",", na.strings = "", header = TRUE, colClasses = colClasses)
-
-  ## Complete the genotype names
-  for (i in 2:nrow(phenoIn)) {
-    if (is.na(phenoIn[i, 1])) {phenoIn[i, 1] <- phenoIn[i - 1, 1]}}
   names(phenoIn)[1] <- "genotype"
 
   ## Remove the accessions which are not in genotypes
+  excludeGenotypes <- unique(phenoIn$genotype[which(!phenoIn$genotype %in% gData$genotypes)])
   phenoOut <- phenoIn[which(phenoIn$genotype %in% gData$genotypes), ]
 
-  ## Add extra accessions, i.e. the ones that do not occur in phenoOut, but DO occur in genotypes
-  extraAcc <- setdiff(gData$genotypes, phenoOut$genotype)
-  if (length(extraAcc) > 0) {
-    phenoOut <- dplyr::add_row(phenoOut, genotype = extraAcc)
-  }
-
-  ## Sort to match sorting done automatically by table command
-  phenoOut <- dplyr::arrange(phenoOut, by = genotype)
-
-  ## Count the number of replicates in the remaining genotypes
-  ## (regardless of missing values in the phenotype)
-  replicates <- as.numeric(table(phenoOut$genotype))
-
-  ## Add _1, _2, etc to the row names to make them unique over replicates
-  row.names(phenoOut) <- paste0(phenoOut$genotype, "_" ,
-    unlist(sapply(replicates, FUN = function(n) {seq(1:n)})))
-
-  ## Reorder by order in genotypes
-  dplyr::right_join(phenoOut, data.frame(genotype = replicates), by = "genotype")
-
-
-
-  if (addVarMeans) {
-    NC    <- ncol(phenoOut)
-    phenoOut <- AddMeans(input.frame=phenoOut,col.select=meanCols)   # the averages of the columns given in col.select are now added as extra columns
-    if (addNormalTransform) {
-      for (i in c(meanCols,NC+1:length(meanCols))) {
-        phenoOut <- cbind(phenoOut,qqnorm(phenoOut[,i],plot.it=F)$x)
-        names(phenoOut)[ncol(phenoOut)] <- paste(names(phenoOut)[i],"_transformed",sep="")
-      }
-    }
-  }
-
-  if (!is.null(factorCols)) {
-    for (i in factorCols) {
-      phenoOut[is.na(phenoOut[, i]), i] <- levels(phenoOut[, i])[1]
-    }
-  }
+  ## Order by order in genotypes
+  dplyr::right_join(phenoOut, data.frame(genotype = gData$genotypes), by = "genotype")
 
   gData$pheno <- phenoOut
 
