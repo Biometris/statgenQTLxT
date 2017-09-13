407f2b646fc3ab5021cd84f9b7253fdcde979271
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2017-09-13 08:57:45 +0200

    Modified examples_SGP

--- examples_SGP.R
+++ examples_SGP.R
@@ -1,23 +1,32 @@
-load('./example_data_drops/atwell_regmap.RData')
+rm(list = ls())
+load("./example_data_drops/testData.Rdata")
 
-map <- GWAS.obj$map[, 1:2]
-colnames(map) <- c('chr', 'pos')
-rownames(map) <- rownames(GWAS.obj$markers)
+colnames(map)[1:2] <- c("chr", "pos")
+pheno <- tibble::rownames_to_column(Y, var = "genotype")
+#gData <- createGData(map=map, geno=t(markers), kin = K, pheno = pheno, covar = NULL)
 
-pheno <- matrix(rnorm(n = 3 * GWAS.obj$n), ncol = 3)
-pheno <- data.frame(genotype = GWAS.obj$plant.names, pheno)
-names(pheno)[-1] <- paste0('trait', 1:3)
+gData0 <- createGData(map=map[map$chr %in% c(2,3,4),],
+  geno=t(markers[rownames(markers) %in% rownames(map[map$chr %in% c(2,3,4),]),]),
+  kin = K, pheno = pheno, covar = NULL)
 
-g <- createGData(geno = t(GWAS.obj$markers), map = map, pheno = pheno)
+covar <- as.data.frame(x = as.factor(sapply(strsplit(gData0$pheno[[1]]$genotype, "_"), tail, 1)))
+colnames(covar) <- "region"
+rownames(covar) <- gData0$pheno[[1]]$genotype
+covar$altitude <- runif(nrow(covar), 0, 2000)
 
-rm(list = setdiff(ls(), g))
+# For fast multitrait GWAS
+Y1 <- gData0$pheno[[1]][, 1:5]
 
+## Create testdata
+gDataTest <- createGData(gData0, pheno = Y1, covar = covar)
+rm(list = setdiff(ls(), c("gDataTest")))
 
-test.gwas1 <- runSingleTraitGwas(gData = g)
-# function unknown !
+## Run GWAS
+testGwas1 <- runSingleTraitGwas(gData = gDataTest)
+testGwas2 <- runMultiTraitGwas(gData = gDataTest, subsetMarkers = TRUE, markerSubset = 40501:41500)
 
-test.gwas2 <- runMultiTraitGwas(gData = g)
-#  Error in markersRed[rownames(Y), rownames(mapRed)] :
-# subscript out of bounds
-
+## summary and plot
+summary(testGwas1)
+plot(testGwas1, trait = "anthesis.ARIHAS_2013_drought")
+plot(testGwas1, trait = "anthesis.ARIHAS_2013_drought", type = "qq")
 
