657859a6f4ceaff2a586c036867114a167df84a3
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2018-10-08 15:15:09 +0200

    Improved genomicPrediction based on SES example 2017

--- R/genomicPrediction.R
+++ R/genomicPrediction.R
@@ -23,9 +23,12 @@ genomicPrediction <- function(gData,
                               traits = NULL,
                               environments = NULL,
                               training,
-                              kin = NULL,
-                              kinshipMethod = c("astle", "GRM", "IBS",
-                                                "vanRaden"),
+                              # kin = NULL,
+                              # kinshipMethod = c("astle", "GRM", "IBS",
+                              #                   "vanRaden"),
+                              useMAF = TRUE,
+                              MAF = 0.01,
+                              MAC = 10,
                               keep = NULL) {
   ## If environments is null set environments to all environments in pheno.
   if (is.null(environments)) {
@@ -33,6 +36,13 @@ genomicPrediction <- function(gData,
   }
   if (!is.null(traits) && !is.numeric(traits) && !is.character(traits)) {
     stop("traits should be a numeric or character vector.\n")
+  }
+  if (useMAF) {
+    chkNum(MAF, min = 0, max = 1)
+    MAF <- max(MAF, 1e-6)
+  } else {
+    chkNum(MAC, min = 0)
+    MAC <- max(MAC, 1)
   }
   for (environment in environments) {
     if ((is.character(traits) &&
@@ -43,24 +53,35 @@ genomicPrediction <- function(gData,
       stop("traits should be columns in pheno.\n")
     }
   }
-  if (is.null(gData$kinship) && is.null(kin)) {
-    ## Compute kinship matrix.
-    kinshipMethod <- match.arg(kinshipMethod)
-    KTrain <- do.call(kinshipMethod, list(X = gData$markers[training, ]))
-  } else if (is.null(kin)) {
-    ## Restrict K to training dataset.
-    KTrain <- gData$kinship[training, training]
-  } else if (is.matrix(kin)) {
-    ## Restrict K to training dataset.
-    KTrain <- as(kin, "dsyMatrix")[training, training]
-  } else {
-    ## Restrict K to training dataset.
-    KTrain <- kin[training, training]
+  # if (is.null(gData$kinship) && is.null(kin)) {
+  #   ## Compute kinship matrix.
+  #   kinshipMethod <- match.arg(kinshipMethod)
+  #   KTrain <- kinship(X = gData$markers[training, ], method = kinshipMethod)
+  # } else if (is.null(kin)) {
+  #   ## Restrict K to training dataset.
+  #   KTrain <- gData$kinship[training, training]
+  # } else if (is.matrix(kin)) {
+  #   ## Restrict K to training dataset.
+  #   KTrain <- as(kin, "dsyMatrix")[training, training]
+  # } else {
+  #   ## Restrict K to training dataset.
+  #   KTrain <- kin[training, training]
+  # }
+  ## Compute max value in markers
+  maxScore <- min(max(gData$markers, na.rm = TRUE), 2)
+  allFreq <- Matrix::colMeans(gData$markers[training, ],
+                              na.rm = TRUE) / maxScore
+  if (!useMAF) {
+    MAF <- MAC / (maxScore * length(training)) - 1e-5
   }
-  p <- Matrix::colSums(gData$markers) / (2 * nrow(gData$markers))
-  Z <- scale(gData$markers, center = 2 * p)
+  ## Determine segregating markers.
+  segMarkers <- allFreq >= MAF & allFreq <= (1 - MAF)
+  markersRed <- gData$markers[, segMarkers]
+  p <- Matrix::colSums(markersRed) / (2 * nrow(markersRed))
+  Z <- scale(markersRed, center = 2 * p)
   Z <- Z / (2 * sqrt(sum(p * (1 - p))))
   Z_sel <- Z[training, ]
+  K1 <- tcrossprod(Z_sel)
   ## Extract training data for modeling.
   dataPredict <- gData$pheno[[environments]]
   dataTrain <- dataPredict[dataPredict$genotype %in% training, ]
@@ -69,7 +90,7 @@ genomicPrediction <- function(gData,
     ## Fit mixed model on training data.
     sommerFit <- sommer::mmer2(fixed = as.formula(paste0(trait, "~ 1")),
                                random = ~ g(genotype),
-                               G = list(genotype = KTrain),
+                               G = list(genotype = K1),
                                rcov = ~ units,
                                data = dataTrain,
                                silent = TRUE, date.warning = FALSE)
@@ -91,7 +112,8 @@ genomicPrediction <- function(gData,
     return(pred)
   })
   ## Create basedata with columns to be copied to output data.
-  baseData <- cbind(data.frame(genotype = rownames(gData$covar)),
+  baseData <- cbind(data.frame(genotype = rownames(gData$covar),
+                               stringsAsFactors = FALSE),
                     gData$covar[colnames(gData$covar) %in% keep])
   ## Merge predictions to baseData.
   predTot <- Reduce(f = merge, x = preds, init = baseData)
